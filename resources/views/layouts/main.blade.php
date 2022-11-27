446acdc14ea900e930cff2b26bfe022a4b3cc74b
Jonathan Oliva <10662970+JonaOliv@users.noreply.github.com>
2022-11-27 04:33:54 -0500

    fixing starting machine and testing

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,15 +1,34 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
-        <title>App Name - @yield('title')</title>
-	@section('css-libs')
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="initial-scale=1,user-scalable=no,maximum-scale=1,width=device-width">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+
+        <meta name="theme-color" content="#000000">
+        <meta name="description" content="Map Design">
+        <meta name="author" content="Jonathan Oliva">
+
+        <title>@yield('title')</title>
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	    @section('css-libs')
             This is the css libs section.
         @show
     </head>
     <body> 
+        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        
+        </div>
+
         <div class="container">
             @yield('content')
         </div>
-	@section('js-libs')
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+	    @section('js-libs')
             This is the js libs section.
         @show
     </body>
